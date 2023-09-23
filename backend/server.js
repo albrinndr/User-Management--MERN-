61600566b8ff510c852a556dee375ac3d6fbaff0
Albrin <albrinndr@gmail.com>
2023-09-23 21:00:40 +0530

    Configured error handler
    Using asyncHandler module and created error redirecting routes as
    middlewares.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
 
-import userRoutes from './routes/userRoutes.js'
-dotenv.config()
-const port = process.env.PORT || 5000
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-const app = express()
+dotenv.config();
+const port = process.env.PORT || 5000;
 
-app.use('/api/users', userRoutes)
-app.get('/', (req, res) => res.send("Server is ready!"))
+const app = express();
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => res.send("Server is ready!"));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
