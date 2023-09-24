35f0ea0637b07a85c6ac721c84d91cdb455451cd
Albrin <albrinndr@gmail.com>
2023-09-24 12:47:33 +0530

    Mongodb connection established

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-dotenv.config();
 const port = process.env.PORT || 5000;
+dotenv.config();
+connectDB();
 
 const app = express();
 
