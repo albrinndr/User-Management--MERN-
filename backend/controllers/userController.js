2baa3607faf9e619ee9c1f971a62fc3dba4492b6
Albrin <albrinndr@gmail.com>
2023-09-24 14:56:27 +0530

    Add jwt
    Generated jwt and added cookie.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Auth user/set token
 //route     POST /api/users/auth
@@ -24,6 +25,7 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({ name, email, password });
 
     if (user) {
+        generateToken(res,user._id);
         res.status(200).json({
             _id: user.id,
             name: user.name,
