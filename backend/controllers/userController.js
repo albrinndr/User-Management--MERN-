b881ffb49853c3ef40e90f29f5bc18a0b6cd16c7
Albrin <albrinndr@gmail.com>
2023-09-24 16:43:28 +0530

    Added user auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,14 +55,24 @@ const registerUser = asyncHandler(async (req, res) => {
 //route     POST /api/users/logout
 //@access   Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout user' });
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    });
+    res.status(200).json({ message: 'User logged out' });
 });
 
 // @desc    Get user profile
 //route     GET /api/users/profile
 //@access   Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'User profile' });
+    const user = {
+        _id: req.user._id,
+        name: req.user.name,
+        email: req.user.email
+    };
+
+    res.status(200).json(user);
 });
 
 // @desc    Update user profile
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
@@ -11,8 +12,10 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
