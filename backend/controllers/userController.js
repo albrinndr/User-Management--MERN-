4cc2035b4c3a3f22684ff2b766d2f93fc50ea78f
Albrin <albrinndr@gmail.com>
2023-09-24 15:35:24 +0530

    Login authentication added
    Comparing passwords is done in model as method and called in controller.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,8 +6,22 @@ import generateToken from '../utils/generateToken.js';
 //route     POST /api/users/auth
 //@access   Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth user' });
-})
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+});
 
 // @desc    Register a new user
 //route     POST /api/users 
@@ -25,8 +39,8 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({ name, email, password });
 
     if (user) {
-        generateToken(res,user._id);
-        res.status(200).json({
+        generateToken(res, user._id);
+        res.status(201).json({
             _id: user.id,
             name: user.name,
             email: user.email
@@ -35,28 +49,28 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Invalid user data');
     }
-})
+});
 
 // @desc    Logout user
 //route     POST /api/users/logout
 //@access   Public
 const logoutUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Logout user' });
-})
+});
 
 // @desc    Get user profile
 //route     GET /api/users/profile
 //@access   Private
 const getUserProfile = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'User profile' });
-})
+});
 
 // @desc    Update user profile
 //route     PUT /api/users/profile
 //@access   Private
 const updateUserProfile = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Update user profile' });
-})
+});
 
 export {
     authUser,
@@ -64,4 +78,4 @@ export {
     logoutUser,
     getUserProfile,
     updateUserProfile
-}
+};
