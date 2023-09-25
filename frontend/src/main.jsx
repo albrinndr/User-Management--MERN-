568f5e15a9dd2f302c9016cfd58fcbfbe92051ef
Albrin <albrinndr@gmail.com>
2023-09-25 12:27:45 +0530

    Add redux store
    Redux store and auth,api slices along with the userLogin api slice is
    configured.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,10 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
-import App from './App.jsx';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx';
 import './index.css';
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
@@ -24,7 +26,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
